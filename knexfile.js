6d3b0e03a6bfcb0eb54f344759d4efa3e02731e7
Roger Eduardo de Couto <rogerecouto@gmail.com>
2020-04-05 16:42:41 -0300

    Modificados arquivos de configuraçao

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,16 @@
 // Update with your config settings.
+require('dotenv').config();
 
 module.exports = {
+
 
   development: {
     client: 'postgresql',
     connection: {
-      database: 'helppydb',
-      user:     'postgres',
-      password: 'admin'
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASS
     },
     pool: {
       min: 2,
@@ -25,9 +28,10 @@ module.exports = {
   staging: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASS
     },
     pool: {
       min: 2,
@@ -41,9 +45,10 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASS
     },
     pool: {
       min: 2,
@@ -51,6 +56,9 @@ module.exports = {
     },
     migrations: {
       tableName: 'knex_migrations'
+    },
+    seeds:{
+      directory: './src/database/seeds'
     }
   }
 
